a97e7d03ae2c9ac5ae7b5003b540fda1349d2bde
celcito <celso_ricardo_25@hotmail.com>
2023-07-27 12:59:28 -0300

    feat:inportacao do schema no modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { KnigtsModule } from './knigts/knigts.module';
 
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [KnigtsModule],
+  imports: [KnigtsModule,MongooseModule.forRoot('mongodb://127.0.0.1:27017',{ dbName: 'knigts' })],
 })
 export class AppModule {}
