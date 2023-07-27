0323a3c564f57513a939f3f1edc4497736f526f5
celcito <celso_ricardo_25@hotmail.com>
2023-07-27 17:08:01 -0300

    feat: adicionado e refatorado schemas e teste unitario

--- src/knigts/knigt/knigt.spec.ts
+++ src/knigts/knigt/knigt.spec.ts
@@ -1,7 +1,86 @@
-import { Knigt } from './knigt';
 
-describe('Knigt', () => {
-  it('should be defined', () => {
-    expect(new Knigt()).toBeDefined();
+import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { Knigt, KnigtSchema, KnigtAttributes, Weapon } from '../../schemas/knigt.schema';
+import {KnigtsService} from '../knigts.service'
+//import { MongooseModule } from '@nestjs/mongoose';
+//import { closeMongoConnection, rootMongooseTestModule } from './mongoose-test-module.ts'
+import { Model } from 'mongoose';
+
+describe('Knigt Schema', () => {
+  let knigtModel: Model<Knigt>;
+  let service: KnigtsService;
+
+  const sampleKnigt: Partial<Knigt> = {
+    name: 'Sir Lancelot',
+    nickname: 'Lance',
+    birthday: new Date('1985-07-15'),
+    weapons: [
+      { name: 'Excalibur', mod: 5, attr: 'Holy', equipped: true },
+      { name: 'Lance of Virtue', mod: 3, attr: 'Justice', equipped: false },
+    ],
+    attributes: {
+      strength: 10,
+      dexterity: 8,
+      constitution: 12,
+      intelligence: 14,
+      wisdom: 13,
+      charisma: 16,
+    },
+    keyAttribute: 'charisma',
+  };
+
+/*   beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: 'Knigt', schema: KnigtSchema }]),
+      ],
+      providers: [KnigtsService],
+    }).compile();
+
+    service = module.get<KnigtsService>(KnigtsService);
   });
+ */
+
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: getModelToken(Knigt.name),
+          useValue: {
+            create: jest.fn().mockResolvedValue(sampleKnigt),
+          },
+        },
+      ],
+    }).compile();
+
+    knigtModel = module.get<Model<Knigt>>(getModelToken(Knigt.name));
+  });
+
+
+
+  it('should be defined', () => {
+   expect(new Knigt()).toBeDefined();
+  });
+
+  it('should service  be defined', () => {
+    expect(KnigtsService).toBeDefined();
+  });
+
+
+
+  it('should save a Knigt', async () => {
+    const createSpy = jest.spyOn(knigtModel, 'create');
+    const createdKnigt = await knigtModel.create(sampleKnigt);
+    expect(createSpy).toHaveBeenCalledWith(sampleKnigt);
+    expect(createdKnigt).toEqual(sampleKnigt);
+  });
+
+/*   afterAll(async () => {
+    await closeMongoConnection();
+  });
+ */
+
 });
