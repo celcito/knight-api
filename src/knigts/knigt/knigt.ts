33356d415283fdd174407a2c9fbaaac0103fb995
celcito <celso_ricardo_25@hotmail.com>
2023-07-27 12:56:39 -0300

    feat: configuracao schema mongoose

--- src/knigts/knigt/knigt.ts
+++ src/knigts/knigt/knigt.ts
@@ -1,1 +1,52 @@
-export class Knigt {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+
+interface Weapon {
+  name: string;
+  mod: number;
+  attr: string;
+  equipped: boolean;
+}
+
+interface KnigtAttibutes {
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+}
+
+
+@Schema()
+export class Knigt extends Document {
+
+@Prop()
+
+name:string;
+
+@Prop()
+
+nickname:string;
+
+@Prop()
+
+birthday:Date;
+
+ @Prop({ type: [{ type: Object }] })
+
+weapons:Weapon[]
+
+@Prop({ type: Object })
+
+attributes:KnigtAttibutes;
+
+@Prop()
+
+keyAttribute:string;
+
+}
+
+
+export const KnigtSchema = SchemaFactory.createForClass(Knigt);
