2cd49bdedc489aa023b5679ae45ad4dda21ac2b1
celcito <celso_ricardo_25@hotmail.com>
2023-07-27 12:55:38 -0300

    feat: criacao conexao com banco mongodb

--- src/knigts/knigts.module.ts
+++ src/knigts/knigts.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { KnigtsService } from './knigts.service';
 import { KnigtsController } from './knigts.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {Knigt,KnigtSchema} from "./knigt/knigt"
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Knigt.name,
+        schema: KnigtSchema
+      }
+    ])
+  ],
   providers: [KnigtsService],
   controllers: [KnigtsController]
 })
