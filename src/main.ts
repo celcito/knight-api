c59cf9e9a400123ea3695b4503d4ca0faae065a6
celcito <celso_ricardo_25@hotmail.com>
2023-07-28 18:34:42 -0300

    fix: resolvido problema com cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('knigts')
     .build();
+
+    app.enableCors();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
