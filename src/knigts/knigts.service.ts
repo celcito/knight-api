a3bfcd240e88546c3a008a9452e7acf20d06a353
celcito <celso_ricardo_25@hotmail.com>
2023-07-27 23:30:29 -0300

    refactor: adicionando try catch

--- src/knigts/knigts.service.ts
+++ src/knigts/knigts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose'
 import {Knigt} from './knigt/knigt'
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 
 
 @Injectable()
@@ -11,24 +12,49 @@ export class KnigtsService {
  ) {}
  
   async searchAll(): Promise<Knigt[]> {
-    return this.knigtModel.find().exec();
+
+
+    try {
+      return this.knigtModel.find().exec();
+    } catch (error) {
+      return null;
+    }
+
   }
 
   async create(knigt: Knigt): Promise<Knigt> {
+    try {
     const knigtCreated = new this.knigtModel(knigt);
     return knigtCreated.save();
+
+    } catch(error){
+      throw new HttpException('Erro ao criar  Knigt', HttpStatus.BAD_REQUEST);
+    }
   }
 
+
   async searchById(id: string): Promise<Knigt> {
-    return this.knigtModel.findById(id).exec();
+      try {
+        return this.knigtModel.findById(id).exec() || null
+      } catch (error) {
+        return null;
+      }
   }
 
   async updateKnigts(id: string, knigt: Knigt): Promise<Knigt> {
-    return this.knigtModel.findByIdAndUpdate(id, knigt,{ "new": true}).exec();
+    try {
+      return this.knigtModel.findByIdAndUpdate(id, knigt,{ "new": true}).exec();
+    } catch (error) {
+      throw new HttpException('Erro ao atualizar o knigt', HttpStatus.BAD_REQUEST);
+    }
   }
 
   async deleteKnigt(id: string) {
-    const knigtRemoved = this.knigtModel.findOneAndDelete({ _id: id }).exec();
-    return await knigtRemoved;
+    try {
+      const knigtRemoved = this.knigtModel.findOneAndDelete({ _id: id }).exec();
+      return await knigtRemoved;    
+    } catch (error) {
+      throw new HttpException('Erro ao atualizar o knigt', HttpStatus.BAD_REQUEST);
+    }
   } 
 }
