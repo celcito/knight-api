7a3c5820babe28bf489872c4ef2a36cf0cf34e5e
celcito <celso832@gmail.com>
2024-02-27 22:06:16 -0300

    fix:resolve bd conection

--- src/knigts/knigt/mongoose-test-module.ts
+++ src/knigts/knigt/mongoose-test-module.ts
@@ -1,23 +1,11 @@
 import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import { disconnect } from 'mongoose';
 
-let mongo: MongoMemoryServer;
-
 export const rootMongooseTestModule = (options: MongooseModuleOptions = {}) =>
-  MongooseModule.forRootAsync({
-    useFactory: async () => {
-      mongo = await MongoMemoryServer.create();
-      const mongoUri = mongo.getUri();
-      return {
-        uri: mongoUri,
-        useCreateIndex: true,
-        ...options,
-      };
-    },
+  MongooseModule.forRoot('mongodb://127.0.0.1:27017/knigts', {
+    ...options,
   });
 
 export const closeMongoConnection = async () => {
   await disconnect();
-  if (mongo) await mongo.stop();
 };
